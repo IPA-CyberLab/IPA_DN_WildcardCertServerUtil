ce62b92f943f100db0622c662e7ced5fb8df4a25
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-06-02 19:59:28 +0900

    initial

---
+++ _new_file.py
@@ -0,0 +1,12 @@
+﻿# IPA-DN-PyNeko-v1
+# 
+# Copyright (c) 2021- IPA CyberLab.
+# All Rights Reserved.
+# 
+# Author: Daiyuu Nobori
+# Description
+
+
+from submodules.IPA_DN_PyNeko.v1.PyNeko import *
+
+
