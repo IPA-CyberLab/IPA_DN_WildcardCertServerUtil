38bd786fa8b931c49642279f7c25c1a2aa4c27fc
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-06-02 21:04:34 +0900

    途中さん

--- Main.py
+++ Main.py
@@ -6,10 +6,20 @@
 # Author: Daiyuu Nobori
 # Description
 
+import subprocess
 
 from submodules.IPA_DN_PyNeko.v1.PyNeko import *
 
 
-Process.ShellExecute("ls -la")
+res = EasyExec.ShellExecutePiped("sleep 5", ignoreError=True, timeoutSecs=1)
+
+print("code = " + str(res.ExitCode))
+
+lines = res.StdOutAndErr.splitlines()
+for line in lines:
+    print(F"# {line}")
+
+
+
 
 
