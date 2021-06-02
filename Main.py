323221f24eb75fa688effc7ccb7f36ad4cae4c82
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-06-02 21:37:31 +0900

    途中さん

--- Main.py
+++ Main.py
@@ -9,15 +9,16 @@
 from submodules.IPA_DN_PyNeko.v1.PyNeko import *
 
 
-res = EasyExec.ShellExecutePiped("sleep 5", ignoreError=True, timeoutSecs=6)
+# res = EasyExec.ShellExecutePiped("sleep 5", ignoreError=True, timeoutSecs=6)
 
-print("code = " + str(res.ExitCode))
+# print("code = " + str(res.ExitCode))
 
-lines = res.StdOutAndErr.splitlines()
-for line in lines:
-    print(F"# {line}")
+# lines = res.StdOutAndErr.splitlines()
+# for line in lines:
+#     print(F"# {line}")
 
 
+print(f"'{s}'")
 
 
 
